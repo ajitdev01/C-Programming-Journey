3ce39d168a2d5e0470340c6d8dfd14c7da450594
Ajit Kumar Sah <ajitk23192@gmail.com>
2026-01-03 08:13:22 +0530

    Add files via upload
    Introduces logical operators (AND, OR, NOT) used to combine and evaluate multiple conditions.

---
+++ seven.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 5;
+
+    printf("Logical Operators Example\n\n");
+
+    printf("(a > 5 && b < 10) : %d\n", (a > 5 && b < 10));
+    printf("(a > 5 || b > 10) : %d\n", (a > 5 || b > 10));
+    printf("!(a == b)        : %d\n", !(a == b));
+
+    return 0;
+}
