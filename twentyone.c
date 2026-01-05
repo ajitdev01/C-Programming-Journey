bd123204d47bb0ab08931ea2a38bc1abcee4e0a8
Ajit Kumar Sah <ajitk23192@gmail.com>
2026-01-05 10:52:44 +0530

    Add files via upload
    This program performs basic input and output operations in C by accepting two integers from the user and calculating their sum. It introduces the use of scanf for input, printf for output, and simple arithmetic operations. This program is ideal for beginners to understand how user interaction and variable manipulation work in C.

---
+++ twentyone.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter num 1 numbers :- ");
+    scanf("%d", &a);
+    printf("Enter num 2 numbers:- ");
+    scanf("%d", &b);
+
+    printf("Sum = %d", a + b);
+    return 0;
+}
