52fcb5eb0d005b785714dbef746fcc78926b9221
Ajit Kumar Sah <ajitk23192@gmail.com>
2026-01-06 11:16:47 +0530

    Add files via upload
    This program demonstrates separating code into multiple files using header files. It improves organization and is widely used in real-world C projects

---
+++ thirtynine.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+     int num;
+    printf("Enter Number You Need squre :- ");
+    scanf("%d", &num);
+    printf("Square root of %d = %.0f\n",num, sqrt(num));
+    return 0;
+}
